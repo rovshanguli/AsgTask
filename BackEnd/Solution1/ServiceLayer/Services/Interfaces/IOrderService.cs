c15fd817c88e137e7ed104035da92b67b4a9e574
Rovshan Guliyev <rovshanshg@code.edu.az>
2022-10-11 15:36:55 +0400

    Migration

---
+++ BackEnd/Solution1/ServiceLayer/Services/Interfaces/IOrderService.cs
@@ -0,0 +1,26 @@
+﻿using ServiceLayer.DTOs.CityDto;
+using ServiceLayer.DTOs.CountryDto;
+using ServiceLayer.DTOs.OrderDto;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceLayer.Services.Interfaces
+{
+    public interface IOrderService
+    {
+        Task CreateAsync(OrderDto orderDto);
+
+        Task UpdateAsync(int Id, OrderEditDto orderEditDto);
+        Task DeleteAsync(int id);
+        Task<List<OrderDto>> GetAllAsync();
+        Task<OrderDto> GetAsync(int id);
+        Task<OrderDto> GetByIdAsync(int id);
+
+
+        //Task<IEnumerable<OrderDto>> GetAllNameAsync(string name);
+
+    }
+}
